ebf117faa9257c7fc274d639eeac5924785df242
Reza-a-nazari <rezanazari.phone@gmail.com>
2025-09-26 14:27:17 +0330

    #3

--- S2/codes/CSHARP_TRAINING/Sort.cs
+++ S2/codes/CSHARP_TRAINING/Sort.cs
@@ -1,5 +1,6 @@
 namespace CSHARP_TRAINING;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 
 
@@ -8,21 +9,46 @@ public class Sort
     public static void Main(string[] args)
     {
 
-        int[] numming = { 1, -1, -4, 10, 11, 245 };
+        System.Console.WriteLine("This program take a long , please wait...!");
 
-        int[] mynum = Bubble_Sort(numming);
+            Random rnd = new Random();
+
+            int[] numming = new int[100000];
+            for (int i = 0; i < numming.Length; i++)
+            {
+                numming[i] = rnd.Next(-100000, 100001);
+            }
+
+
 
         Stopwatch t1 = new Stopwatch();
 
         t1.Start();
-        foreach (var i in mynum)
-        {
-            System.Console.WriteLine(i);
-        }
+
+        int[] mynum = Bubble_Sort(numming);
 
         t1.Stop();
 
-        System.Console.WriteLine("The time span is : " +t1.ElapsedMilliseconds.ToString() + " millisecond");
+        System.Console.WriteLine("The time span is : " + t1.ElapsedMilliseconds.ToString() + " millisecond" + " Bubble sort");
+
+
+        Stopwatch t2 = new Stopwatch();
+        List<int> nums = new List<int>(numming);
+
+
+        t2.Start();
+
+        List<int> Ans = Selection_Sort(nums);
+
+        // foreach (var i in Ans)
+        // {
+        //     System.Console.WriteLine(i);
+        // }
+
+        t2.Stop();
+
+        System.Console.WriteLine("The time span is : " + t2.ElapsedMilliseconds.ToString() + " millisecond" + " Selection sort");
+        
 
     }
 
@@ -46,10 +72,32 @@ public class Sort
         return nums;
     }
 
-    // public static int[] Selection_Sort(int[] nums)
-    // {
+    public static List<int> Selection_Sort(List<int> nums)
+    {
+        List<int> final = new List<int>();
 
-    // }
+
+        while (nums.Count > 0)
+        {
+            int min = nums[0];
+
+            int MinIndex = 0;
+
+            for (int i = 1; i < nums.Count; i++)
+            {
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                    MinIndex = i;
+                }
+            }
+
+            final.Add(min);
+
+            nums.RemoveAt(MinIndex);
+        }
+        return final;
+    }
 
     // public static int[] Insertion_Sort(int[] nums)
     // {
--- S2/codes/CSHARP_TRAINING/obj/Debug/net9.0/CSHARP_TRAINING.AssemblyInfo.cs
+++ S2/codes/CSHARP_TRAINING/obj/Debug/net9.0/CSHARP_TRAINING.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f701f4ea792ab80c326c8f9913840079687bc00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac1e0298f158b2fcbae88722a35edc1da91ed230")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
