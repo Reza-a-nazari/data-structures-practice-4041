bc1d7490321b1cc41f9562fc678114b56bf0a474
Reza-a-nazari <rezanazari.phone@gmail.com>
2025-10-02 01:04:58 +0330

    #10/2/2025

--- S2/codes/CSHARP_TRAINING/obj/Debug/net9.0/CSHARP_TRAINING.AssemblyInfo.cs
+++ S2/codes/CSHARP_TRAINING/obj/Debug/net9.0/CSHARP_TRAINING.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2882b5f89c298de6e39a57d512e2b25a1bf1cdf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917908f2a66841360595b22cadf844b9a4cedaae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARP_TRAINING")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
